62393d96d5ea3e1de22cecd5fc4a7bd0c5ddf6d3
Wayne Yip <wqyip97@gmail.com>
2024-11-14 11:41:23 +0800

    Create simple menu w/ input and responses

--- NotesApp.cs
+++ NotesApp.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hi World!");
+﻿class NotesApp()
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please select an option:");
+        Console.WriteLine("1. Create New Note");
+        Console.WriteLine("2. View Notes");
+        Console.WriteLine("3. Delete Note");
+
+        string userInput = Console.ReadLine();
+        switch (userInput)
+        {
+            case "1":
+                Console.WriteLine("New Note");
+                break;
+            case "2":
+                Console.WriteLine("View Notes");
+                break;
+            case "3":
+                Console.WriteLine("Delete Note");
+                break;
+            default:
+                Console.WriteLine("Invalid choice - please enter the number for your option (1, 2, or 3)");
+                break;
+        }
+    }
+}
